1ced0f0307d3a0e85a64e07a722d84141d5728cb
Nicholas Kennedy <nmk10062@gmail.com>
2024-08-31 00:13:58 -0400

    Fix for when min is greater than max

--- app/number/random/RandomNumberGenerator.tsx
+++ app/number/random/RandomNumberGenerator.tsx
@@ -10,7 +10,17 @@ const RandomNumberGenerator = () => {
   const [max, setMax] = useState("100");
   const [random, setRandom] = useState("0");
 
+  const orderNumbers = () => {
+    if (parseInt(min) < parseInt(max) + 1) return;
+
+    const temp = min;
+    setMin(max);
+    setMax(temp);
+  };
+
   const generateRandomInt = () => {
+    orderNumbers();
+
     const minCeiled = Math.ceil(parseInt(min));
     const maxFloored = Math.floor(parseInt(max) + 1);
 
@@ -23,29 +33,31 @@ const RandomNumberGenerator = () => {
 
   return (
     <>
-      <div className="flex justify-between items-center gap-4">
-        <Input
-          type="number"
-          placeholder="Minimum"
-          min={Number.MIN_SAFE_INTEGER}
-          max={Number.MAX_SAFE_INTEGER - 1}
-          className="w-32"
-          value={min}
-          onChange={(e) => setMin(e.target.value)}
-        />
-        <span>To</span>
-        <Input
-          type="number"
-          placeholder="Maximum"
-          min={Number.MIN_SAFE_INTEGER}
-          max={Number.MAX_SAFE_INTEGER - 1}
-          className="w-32"
-          value={max}
-          onChange={(e) => setMax(e.target.value)}
-        />
+      <div className="flex gap-4">
+        <div className="flex justify-between items-center gap-4">
+          <Input
+            type="number"
+            placeholder="Minimum"
+            min={Number.MIN_SAFE_INTEGER}
+            max={max}
+            className="w-32"
+            value={min}
+            onChange={(e) => setMin(e.target.value)}
+          />
+          <span>To</span>
+          <Input
+            type="number"
+            placeholder="Maximum"
+            min={min}
+            max={Number.MAX_SAFE_INTEGER - 1}
+            className="w-32"
+            value={max}
+            onChange={(e) => setMax(e.target.value)}
+          />
+        </div>
       </div>
       <div className="relative w-full">
-        <h1 className="w-full p-2 font-bold rounded-lg border text-[3rem]">
+        <h1 className="w-full p-2 font-bold rounded-lg border text-[2.5rem]">
           {random}
         </h1>
         <CopyButton copyValue={random} />
