9b337c30c246fb653d01df5b86db8634ea7810eb
Nicholas Kennedy <nmk10062@gmail.com>
2024-09-05 16:03:14 -0400

    Update header text color

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -7,7 +7,7 @@ const Header = ({ title, description }: Props) => {
   return (
     <header className="w-full py-12">
       <div className="flex flex-col items-center text-center container">
-        <h1 className="text-3xl font-bold tracking-tighter md:text-4xl">
+        <h1 className="text-primary text-3xl font-bold tracking-tighter md:text-4xl">
           {title}
         </h1>
         <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
