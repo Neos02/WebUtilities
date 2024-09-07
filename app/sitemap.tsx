16b9650dd10a3f5ca7a2d8cc4a42b9d6722359a7
Nicholas Kennedy <nmk10062@gmail.com>
2024-09-07 12:32:05 -0400

    Make sitemap read from page routes

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,39 +1,58 @@
 import { MetadataRoute } from "next";
+import { Page, pageDetails } from "./navigation";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: new URL("/", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.Home].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: new URL("/about", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.About].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: new URL("/color/picker", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.ColorPicker].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: new URL("/number/convert", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.BaseConverter].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: new URL("/number/random", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.RandomNumberGenerator].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: new URL("/text/count", process.env.NEXT_PUBLIC_URL!).toString(),
+      url: new URL(
+        pageDetails[Page.WordCounter].href,
+        process.env.NEXT_PUBLIC_URL!
+      ).toString(),
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
