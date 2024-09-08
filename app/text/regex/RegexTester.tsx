a1ff96cd2161b890783753c210226464a93a719a
Nicholas Kennedy <nmk10062@gmail.com>
2024-09-07 20:03:51 -0400

    Create regex tester component

---
+++ app/text/regex/RegexTester.tsx
@@ -0,0 +1,11 @@
+import ToolContainer from "@/components/layout/ToolContainer";
+
+const RegexTester = () => {
+  return (
+    <ToolContainer>
+      <h1>Regex</h1>
+    </ToolContainer>
+  );
+};
+
+export default RegexTester;
--- app/text/regex/page.tsx
+++ app/text/regex/page.tsx
@@ -1,6 +1,6 @@
 import { Page, pageDetails } from "@/app/navigation";
-import ToolContainer from "@/components/layout/ToolContainer";
 import ToolLayout from "@/components/layout/ToolLayout";
+import RegexTester from "./RegexTester";
 
 const page = () => {
   return (
@@ -8,9 +8,7 @@ const page = () => {
       title={pageDetails[Page.RegexTester].label}
       description={pageDetails[Page.RegexTester].description}
     >
-      <ToolContainer>
-        <h1>Regex</h1>
-      </ToolContainer>
+      <RegexTester />
     </ToolLayout>
   );
 };
