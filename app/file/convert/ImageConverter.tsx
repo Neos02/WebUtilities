00d1b3bf77f3295ffd414b836b3c41ff1f3ba793
Nicholas Kennedy <nmk10062@gmail.com>
2024-09-23 16:33:34 -0400

    Show image preview

--- app/file/convert/ImageConverter.tsx
+++ app/file/convert/ImageConverter.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -11,6 +19,7 @@ import {
 } from "@/components/ui/select";
 import { SelectValue } from "@radix-ui/react-select";
 import { extension } from "mime-types";
+import { default as NextImage } from "next/image";
 import { useRef, useState } from "react";
 
 enum ImageType {
@@ -23,6 +32,9 @@ const ImageConverter = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [input, setInput] = useState<File>();
   const [typeTo, setTypeTo] = useState(ImageType.JPEG);
+  const [outputUrl, setOutputUrl] = useState("");
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
 
   const convert = async () => {
     if (!input) {
@@ -41,11 +53,11 @@ const ImageConverter = () => {
 
           canvasRef.current.width = image.width;
           canvasRef.current.height = image.height;
+          setWidth(image.width);
+          setHeight(image.height);
 
           ctx.drawImage(image, 0, 0);
-          const dataUrl = canvasRef.current.toDataURL(typeTo, 1);
-
-          download(dataUrl);
+          setOutputUrl(canvasRef.current.toDataURL(typeTo, 1));
         }
       };
 
@@ -55,50 +67,87 @@ const ImageConverter = () => {
     fr.readAsDataURL(input);
   };
 
-  const download = (url: string) => {
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `test.${extension(typeTo)}`;
-    a.click();
-    a.remove();
+  const getFilename = () => {
+    return input?.name.replace(/\..+$/, "");
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <canvas ref={canvasRef} hidden />
+    <>
+      <div className="flex flex-col gap-4">
+        <canvas ref={canvasRef} hidden />
 
-      <div className="flex gap-4">
-        <div className="flex-1 flex flex-col gap-2">
-          <Label htmlFor="imageInput">Image</Label>
-          <Input
-            id="imageInput"
-            type="file"
-            accept={Object.values(ImageType).join(",")}
-            onChange={(e) => setInput(e.target.files?.[0])}
-          />
+        <div className="flex gap-4">
+          <div className="flex-1 flex flex-col gap-2">
+            <Label htmlFor="imageInput">Image</Label>
+            <Input
+              id="imageInput"
+              type="file"
+              accept={Object.values(ImageType).join(",")}
+              onChange={(e) => setInput(e.target.files?.[0])}
+            />
+          </div>
+          <div className="flex flex-col gap-2 min-w-32">
+            <Label htmlFor="imageInput">Convert To</Label>
+            <Select
+              value={typeTo}
+              onValueChange={(value) => setTypeTo(value as ImageType)}
+            >
+              <SelectTrigger id="imageInput">
+                <SelectValue placeholder="Convert To" />
+              </SelectTrigger>
+              <SelectContent>
+                {Object.entries(ImageType).map(([type, mime]) => (
+                  <SelectItem key={type} value={mime}>
+                    {type}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 min-w-32">
-          <Label htmlFor="imageInput">Convert To</Label>
-          <Select
-            value={typeTo}
-            onValueChange={(value) => setTypeTo(value as ImageType)}
-          >
-            <SelectTrigger id="imageInput">
-              <SelectValue placeholder="Convert To" />
-            </SelectTrigger>
-            <SelectContent>
-              {Object.entries(ImageType).map(([type, mime]) => (
-                <SelectItem key={type} value={mime}>
-                  {type}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
+
+        <Button onClick={convert}>Convert</Button>
       </div>
 
-      <Button onClick={convert}>Convert</Button>
-    </div>
+      <Dialog open={!!outputUrl} onOpenChange={() => setOutputUrl("")}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Preview Image</DialogTitle>
+            <DialogDescription>{`${getFilename()}.${extension(
+              typeTo
+            )}`}</DialogDescription>
+          </DialogHeader>
+          <div className="w-full aspect-square">
+            {outputUrl && (
+              <NextImage
+                className="w-full h-full object-contain"
+                src={outputUrl}
+                width={width}
+                height={height}
+                alt="Converted Image"
+              />
+            )}
+          </div>
+          <DialogFooter className="flex gap-2">
+            <Button
+              className="flex-1"
+              variant={"outline"}
+              onClick={() => setOutputUrl("")}
+            >
+              Close
+            </Button>
+            <Button className="flex-1 !m-0" asChild>
+              <a
+                href={outputUrl}
+                download={`${getFilename()}.${extension(typeTo)}`}
+              >
+                Download
+              </a>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
