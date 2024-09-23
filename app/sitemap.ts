6f656929bb62ba8d82d81982b583339a6263a922
Nicholas Kennedy <nmk10062@gmail.com>
2024-09-23 16:48:04 -0400

    Autogenerate sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,61 +1,15 @@
 import { MetadataRoute } from "next";
-import { Page, pageDetails } from "./navigation";
+import { pageDetails } from "./navigation";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
-      url: new URL(
-        pageDetails[Page.Home].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: new URL(
-        pageDetails[Page.About].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
-    },
-    {
-      url: new URL(
-        pageDetails[Page.ColorPicker].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: new URL(
-        pageDetails[Page.BaseConverter].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: new URL(
-        pageDetails[Page.RandomNumberGenerator].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: new URL(
-        pageDetails[Page.WordCounter].href,
-        process.env.NEXT_PUBLIC_URL!
-      ).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
+    ...Object.values(pageDetails).map(
+      (details): MetadataRoute.Sitemap[number] => ({
+        url: new URL(details.href, process.env.NEXT_PUBLIC_URL!).toString(),
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 1,
+      })
+    ),
   ];
 }
